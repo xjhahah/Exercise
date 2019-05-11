ac588fa8345059c87006a3d593f68f10a2219896
xjh <908234867@qq.com>
2019-05-11 21:36:44 +0800

    add code

---
+++ LeetCode/LeetCode75/main.cc
@@ -0,0 +1,97 @@
+#if 0
+给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+注意:
+不能使用代码库中的排序函数来解决这道题。
+示例:
+输入: [2,0,2,1,1,0]
+输出: [0,0,1,1,2,2]
+#endif
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int vv[3]={0};
+        int v[3]={0,1,2};
+        
+        for(int i=0;i<nums.size();++i){
+            if(nums[i]==0)
+                vv[0]++;
+            if(nums[i]==1)
+                vv[1]++;
+            if(nums[i]==2)
+                vv[2]++;
+        }
+        int index=0;
+        for(int i=0;i<3;++i)
+        {
+            for(int j=0;j<vv[i];++j)
+            {
+                nums[index] = v[i];
+                index++;
+            }
+        }
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+string integerVectorToString(vector<int> list, int length = -1) {
+    if (length == -1) {
+        length = list.size();
+    }
+
+    if (length == 0) {
+        return "[]";
+    }
+
+    string result;
+    for(int index = 0; index < length; index++) {
+        int number = list[index];
+        result += to_string(number) + ", ";
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+        
+        Solution().sortColors(nums);
+
+        string out = integerVectorToString(nums);
+        cout << out << endl;
+    }
+    return 0;
+}
