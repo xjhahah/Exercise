094e55bee7db775452bd208807e9956651c9160b
xjhahah <908234867@qq.com>
2019-07-31 17:36:26 +0800

    add 岛屿数量 code

---
+++ LeetCode/LeetCode200/main.cc
@@ -0,0 +1,38 @@
+//岛屿数量
+class Solution {
+ public:
+    int pos[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+  void DFS(vector<vector<char>>& v, int row, int col, int x, int y,
+           vector<vector<int>>& book) {
+    book[x][y] = 1;
+    for (int i = 0; i < 4; i++) {
+      int nx = x + pos[i][0];
+      int ny = y + pos[i][1];
+      if (nx >= row || nx < 0 || ny >= col || ny < 0) continue;
+      if (v[nx][ny] == '1' && book[nx][ny] == 0) {
+        DFS(v, row, col, nx, ny, book);
+      }
+    }
+  }
+  int numIslands(vector<vector<char>>& grid) {
+    if (grid.empty()) return 0;
+    int row = grid.size();
+    int col = grid[0].size();
+
+    vector<vector<int>> book;
+    book.resize(row);
+    for (int i = 0; i < row; ++i) {
+      book[i].resize(col, 0);
+    }
+    int num = 0;
+    for (int i = 0; i < row; ++i) {
+      for (int j = 0; j < col; ++j) {
+        if (grid[i][j] == '1' && book[i][j] == 0) {
+          ++num;
+          DFS(grid, row, col, i, j, book);
+        }
+      }
+    }
+    return num;
+  }
+};
