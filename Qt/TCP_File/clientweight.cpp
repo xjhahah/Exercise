9ae2d90ae683e1e7f9884c401e2af4924a393261
xjhahah <908234867@qq.com>
2020-03-05 22:40:52 +0800

    add new code

--- Qt/TCP_File/clientweight.cpp
+++ Qt/TCP_File/clientweight.cpp
@@ -1,6 +1,11 @@
 #include "clientweight.h"
 #include "ui_clientweight.h"
 
+#include <QHostAddress>
+
+#define cout qDebug() << "[" << __FILE__ << __LINE__ << "]"
+
+
 ClientWeight::ClientWeight(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::ClientWeight)
@@ -14,10 +19,33 @@ ClientWeight::ClientWeight(QWidget *parent) :
     connect(tcpSocket,&QTcpSocket::readyRead,
             [=](){
         //取出接收到的内容
-        QString buf = tcpSocket->readAll();
-        if(true == isStart){
+        auto buf = tcpSocket->readAll();
+        if(true == isStart){  //接收头
             isStart = false;
             //解析头部信息
+            //初始化
+            fileName = QString(buf).section("##",0,0);
+            fileSize = QString(buf).section("##",1,1).toInt();
+
+            recvSize = 0;
+
+            //打开文件
+            file.setFileName(fileName);
+            bool flag = file.open(QIODevice::WriteOnly);
+
+            if(flag == false){
+                cout << "文件打开失败...";
+            }
+        }else{  //文件信息
+            auto len = file.write(buf);
+            recvSize += len;
+            if(recvSize == fileSize){
+                file.close();
+                QMessageBox::information(this,"完成","文件接收成功");
+
+                tcpSocket->disconnectFromHost();
+                tcpSocket->close();
+            }
         }
     });
 }
@@ -25,4 +53,13 @@ ClientWeight::ClientWeight(QWidget *parent) :
 ClientWeight::~ClientWeight()
 {
     delete ui;
+}
+
+void ClientWeight::on_buttonConnect_clicked()
+{
+    //获取服务器的IP和端口
+    QString ip = ui->lineEditIP->text();
+    auto port = ui->lineEditPort->text().toInt();
+
+    tcpSocket->connectToHost(QHostAddress(ip),port);
 }
--- Qt/TCP_File/clientweight.h
+++ Qt/TCP_File/clientweight.h
@@ -2,12 +2,14 @@
 #define CLIENTWEIGHT_H
 
 #include <QWidget>
-
+#include <QDebug>
 #include <QString>
 #include <QFile>
 
 #include <QTcpSocket>
+#include <QTcpServer>
 
+#include <QMessageBox>
 
 namespace Ui {
 class ClientWeight;
@@ -21,15 +23,18 @@ public:
     explicit ClientWeight(QWidget *parent = nullptr);
     ~ClientWeight();
 
+private slots:
+    void on_buttonConnect_clicked();
+
 private:
     Ui::ClientWeight *ui;
 
-    QTcpSocket* tcpSocket;
+    QTcpSocket   *tcpSocket;
+    QFile        file; //文件对象
     QString      fileName; //文件名
     qint64       fileSize; //文件大小
     qint64       recvSize; //接收文件的大小
-
-    bool         isStart = true;
+    bool         isStart;
 };
 
 #endif // CLIENTWEIGHT_H
